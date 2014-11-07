f6a1792c4571ff9b907f94c13db9de4f106a8a8b
mimipaskova <maria.paskova3@gmail.com>
2014-11-08 00:17:42 +0200

    [Enhancement] first_page_controller was started.

--- src/public/client.js
+++ src/public/client.js
@@ -1,22 +1,22 @@
 var network = require("network");
 
 var Client = function() {
-	this.socket = null;
+    this.socket = null;
 }
 
 Client.prototype.start = function() {
-	this.socket = io.connect("", { port: 8080, transports: ["websocket"] });
-	
-	this.load("src/public/main.html");
+    this.socket = io.connect("", { port: 8080, transports: ["websocket"] });
+    
+    this.load("src/public/main.html");
 }
 
 Client.prototype.load = function(path) {
-	$("#content").load(path);
+    $("#content").load(path);
 }
 
 var client;
 
 window.onload = function() {
-	client = new Client();
-	client.start();
+    client = new Client();
+    client.start();
 }
---
+++ src/public/first_page_controller.js
@@ -0,0 +1,14 @@
+var FirstPageController = function() {
+};
+
+FirstPageController.run = function() {
+	var name = $("#name").val();
+	console.log(name);
+}
+
+FirstPageController.bindListeners = function() {
+	// $(".fa").oncli
+	console.log("dsadsa");
+}
+
+FirstPageController.bindListeners();
