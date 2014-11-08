40bc9bc26d6c9d093a0559912ed6faf3e23b8654
emVeselin-Genadiev/em <emgenadiev.veselin@gmail.com/em>
2014-11-08 11:14:36 +0200

    [Feature] Add events to get Player and Room info.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
-var main = function() {
+var networking = require("./networking_objects.js");
+
+var Main = function() {
 	this.socket = null;
+	this.rooms = {};
+	this.players = {};
 }
 
-main.prototype.start = function(socket) {
+Main.prototype.firstFreeRoom = function() {
+	for(var i = 0, length = this.rooms.length; i < length; i++) {
+		if(this.rooms[i].isFree()) {
+			return this.rooms[i];
+		}
+	}
+}
+
+Main.prototype.getPlayer = function(data) {
+	if(data && data.playerName) {
+		var player = new networking.Player(data.playerName);
+		this.players[player.id] = player;
+		this.socket.emit("getPlayer", { playerId: player.id, playerName: player.name });
+	}
+}
+
+Main.prototype.getRoom = function(data) {
+	if(data && data.playerId && this.players[data.playerId]) {
+		var room = this.firstFreeRoom() || new networking.Room();
+		this.rooms[room.id] = this.rooms[room.id] || room;
+		this.players[data.playerId].roomId = room.id;
+		room.players.push(this.players[data.playerId]);
+		this.socket.emit("getRoom", { roomId: room.id })
+	}
+}
+
+Main.prototype.start = function(socket) {
 	this.socket = socket;
-	console.log("lqlq");
-	this.socket.emit("connected", {message : "Hi"});
-
+	this.socket.emit("connected", { message: "Hi" });
+	this.socket.on("getRoom", this.getRoom.bind(this));
+	this.socket.on("getPlayer", this.getPlayer.bind(this));
 }
 
-module.exports = new main();
+module.exports = new Main();
