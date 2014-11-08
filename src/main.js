3c90483ba8aa98a9274e764156f55b9a899a3925
Nikolay Stoykov <nikolaisstoykov@gmail.com>
2014-11-08 11:56:07 +0200

    [Bugfix] Fix indentations.

--- src/main.js
+++ src/main.js
@@ -2,48 +2,48 @@ var networking = require("./networking_objects.js");
 var networkManager = require("./network_manager");
 
 var Main = function() {
-	this.sockets = [];
-	this.rooms = {};
-	this.players = {};
+    this.sockets = [];
+    this.rooms = {};
+    this.players = {};
 
     this.masterSocket = null;
 }
 
 Main.prototype.firstFreeRoom = function() {
-	for(var i = 0, length = this.rooms.length; i < length; i++) {
-		if(this.rooms[i].isFree()) {
-			return this.rooms[i];
-		}
-	}
+    for(var i = 0, length = this.rooms.length; i < length; i++) {
+        if(this.rooms[i].isFree()) {
+            return this.rooms[i];
+        }
+    }
 }
 
 Main.prototype.getPlayer = function(socket, data) {
-	if(data && data.playerName) {
-		var player = new networking.Player(data.playerName);
-		this.players[player.id] = player;
-		socket.emit("getPlayer", { playerId: player.id, playerName: player.name });
-	}
+    if(data && data.playerName) {
+        var player = new networking.Player(data.playerName);
+        this.players[player.id] = player;
+        socket.emit("getPlayer", { playerId: player.id, playerName: player.name });
+    }
 }
 
 Main.prototype.getRoom = function(socket, data) {
-	if(data && data.playerId && this.players[data.playerId]) {
-		var room = this.firstFreeRoom() || new networking.Room();
-		this.rooms[room.id] = this.rooms[room.id] || room;
-		this.players[data.playerId].roomId = room.id;
-		room.players.push(this.players[data.playerId]);
-		socket.emit("getRoom", { roomId: room.id })
-	}
+    if(data && data.playerId && this.players[data.playerId]) {
+        var room = this.firstFreeRoom() || new networking.Room();
+        this.rooms[room.id] = this.rooms[room.id] || room;
+        this.players[data.playerId].roomId = room.id;
+        room.players.push(this.players[data.playerId]);
+        socket.emit("getRoom", { roomId: room.id })
+    }
 }
 
 Main.prototype.start = function(io, socket) {
-	this.sockets.push(socket);
+    this.sockets.push(socket);
     this.masterSocket = io;
 
     networkManager.setup(io, socket);
 
-	socket.emit("connected", { message: "Hi" });
-	socket.on("getRoom", this.getRoom.bind(this, socket));
-	socket.on("getPlayer", this.getPlayer.bind(this, socket));
+    socket.emit("connected", { message: "Hi" });
+    socket.on("getRoom", this.getRoom.bind(this, socket));
+    socket.on("getPlayer", this.getPlayer.bind(this, socket));
 }
 
 module.exports = new Main();
--- src/networking_objects.js
+++ src/networking_objects.js
@@ -2,30 +2,30 @@ var networking = networking || {};
 var MAX_PLAYERS = 8;
 
 networking.token = function() {
-	return Math.random().toString(36).substr(2);
+    return Math.random().toString(36).substr(2);
 }
 
 networking.Player = function(name) {
-	this.id = name + networking.token();
-	this.name = name;
-	this.roomId = null;
+    this.id = name + networking.token();
+    this.name = name;
+    this.roomId = null;
 }
 
 networking.Room = function() {
-	this.id = networking.token();
-	this.players = [];
+    this.id = networking.token();
+    this.players = [];
 }
 
 networking.Room.prototype.isFree = function() {
-		return this.players.length < MAX_PLAYERS;
+        return this.players.length < MAX_PLAYERS;
 }
 
 networking.Room.prototype.removePlayer = function(player) {
-	var playerIndex = this.players.indexOf(player)
+    var playerIndex = this.players.indexOf(player)
 
-	if(playerIndex > -1) {
-		this.players.splice(playerIndex)
-	}
+    if(playerIndex > -1) {
+        this.players.splice(playerIndex)
+    }
 }
 
 module.exports = networking;
