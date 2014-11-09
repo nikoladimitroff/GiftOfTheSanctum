132ead36a0cf9b9d84ab035603b3ce4ad23c86aa
nikup <nikupbg@gmail.com>
2014-11-09 16:00:50 +0200

    [Enhancment] Arena and fog images and rendering

--- src/platform.js
+++ src/platform.js
@@ -26,6 +26,8 @@ sanctum.Platform = function (texture, outsideTexture, description) {
     this.collapseIterationsLeft = description.collapseIterations;
     this.collapseInterval = description.collapseInterval;
     this.collapseRadiusReduction = radius / description.collapseIterations;
+    this.width = description.width;
+    this.height = description.height;
 };
 
 
@@ -42,7 +44,7 @@ sanctum.Platform.prototype.update = function (dt) {
 sanctum.Platform.prototype.generateVertices = function (sides, radius) {
     var vertices = [];
     for (i = 0; i < sides; i++) {
-        var angle = Math.PI / 4 + i * 2 * Math.PI / sides;
+        var angle = Math.PI / 10 + i * 2 * Math.PI / sides;
         var nextVertex = new Vector(radius * Math.cos(angle),
                                     radius * Math.sin(angle));
         vertices.push(nextVertex);
